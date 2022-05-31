687da1f2f5a4f75934912d718d8f20f921181fc4
Taylor Guenthner <wtguenthner@gmail.com>
2022-05-31 14:56:47 -0400

    Working color change for the second column

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,5 @@
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-var hours;
 var date = document.getElementById('currentDay')
 var nineInput = document.getElementById('nineInput')
 var tenInput = document.getElementById('tenInput')
@@ -11,6 +10,9 @@ var twoInput = document.getElementById('twoInput')
 var threeInput = document.getElementById('threeInput')
 var fourInput = document.getElementById('fourInput')
 var fiveInput = document.getElementById('fiveInput')
+
+var change = document.getElementsByName('textField');
+// Sets date and places it in an object
 var myDate = new Date();
 const parts = {
         date: myDate.getDate(),
@@ -20,11 +22,26 @@ const parts = {
         hour: myDate.getHours(),
     };
 var hours = parts.hour;
+
+//Displays date
 date.innerHTML = "Today is: " + days[`${parts.day}`] + " " + months[`${parts.month}`] + ' ' + parts.date + ", " + parts.year ;
 
-
-
-function test(){
-
-console.log(nineInput.value);
+for(var x=0; x< 9; x++){
+    var value = change[x].getAttribute("value");
+    if(hours>value){
+        change[x].className = "past";
+    }
+    if(hours == value){
+        change[x].className = "present";
+    }
+    if(hours < value){
+        change[x].className = "future";
+    }
 }
+
+
+
+// change[x].className = "present";
+// var value = change[2].getAttribute("value");
+// console.log(value)
+// console.log(hours)
